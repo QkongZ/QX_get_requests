aff2124328b8387fb5c55022577e6dbda65c8e30
idodo <96138710+QkongZ@users.noreply.github.com>
2023-04-24 00:32:04 +0800

    Update get_cookie_gqcq.js

--- get_ck_js/get_cookie_gqcq.js
+++ get_ck_js/get_cookie_gqcq.js
@@ -1,22 +1,39 @@
 /*
 广汽传祺在QX中抓取的token值是倒序 例如xxx : token
+就在后面添加 .split('').reverse().join('')
+
+token：匹配 token 字符串；
+\s*:\s*：匹配冒号前后的空格；
+([^\s]+)：匹配至少一个非空白字符，并捕获该值。
 */
 const cookieName = '广汽传祺token'
-
-const regexToken = /token:\s?([^\s]+)/i;
-
-let headerToken = $request.headers['token'];
-
+const keyToken = 'token'; // 指定要匹配的请求头中的键名
+const regexToken = /\b([^:]+)\s*:\s*token\s*(.*)/i;
+let headerToken = '';
+for (const key in $request.headers) {
+  if (key.toLowerCase().endsWith(': token')) {
+    headerToken = $request.headers[key];
+    break;
+  }
+}
 if (headerToken) {
-  let token = regexToken.exec(headerToken)[1].split('').reverse().join('');
-  console.log(`${cookieName}: Token=${headerToken}`);
-  $notify(`${cookieName}`, '', `Token=${headerToken}`);
+  const matches = regexToken.exec(headerToken);
+  if (matches && matches.length >= 3) {
+    const token = matches[2];
+    console.log(`获取到的token值为：${token}`);
+    $notify('获取Token成功', '', `Token=${token}`);
+  } else {
+    console.log('未匹配到Token');
+    $notify('获取Token失败', '', '未匹配到Token');
+  }
 } else {
-  $notify(cookieName, '获取token失败', '请检查请求头中是否包含token');
+  console.log('请求头中未包含Token');
+  $notify('获取Token失败', '', '请求头中未包含Token');
 }
 
 
-$notify('广汽传祺token获取成功！', '', `${cookieName}获取成功！请查看日志或弹窗获取Cookie信息。`)
+
+$notify('广汽传祺token获取成功！', '', `${cookieName}获取成功！请查看日志或弹窗获取token信息。`)
 console.log(`${cookieName}获取成功！`)
 console.log(`Cookie：${headerToken}`)
 
