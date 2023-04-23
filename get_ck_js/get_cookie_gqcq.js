6f91a051936ce16c4c97acdbaffd01eb4edb9712
idodo <96138710+QkongZ@users.noreply.github.com>
2023-04-23 23:35:58 +0800

    Update get_cookie_gqcq.js

--- get_ck_js/get_cookie_gqcq.js
+++ get_ck_js/get_cookie_gqcq.js
@@ -1,27 +1,22 @@
 /*
-获取请求头中的token，正则匹配区分大小写为token
-
-const keyToken = 'token'; 使用 keyToken 来表示 token 的键名
-
-const regexToken = new RegExp(`${keyToken}=([^;]+)`); 使用 RegExp 构造函数创建一个正则表达式对象来匹配对应键名的值。
-
-"token=" + token
+广汽传祺在QX中抓取的token值是倒序 例如xxx : token
 */
 const cookieName = '广汽传祺token'
 
 const regexToken = /token:\s?([^\s]+)/i;
 
-let headerToken = $request.headers['token']
+let headerToken = $request.headers['token'];
 
 if (headerToken) {
-  let token = regexToken.exec(headerToken)[1];
-  console.log(`${cookieName}: Token: ${token}`);
-  $notify(`${cookieName}`, '', `token=${headerToken}`);
+  let token = regexToken.exec(headerToken)[1].split('').reverse().join('');
+  console.log(`${cookieName}: Token=${headerToken}`);
+  $notify(`${cookieName}`, '', `Token=${headerToken}`);
 } else {
   $notify(cookieName, '获取token失败', '请检查请求头中是否包含token');
 }
 
-$notify('广汽传祺token获取成功！', '', `${cookieName}获取成功！请查看日志或弹窗获取token信息。`)
+
+$notify('广汽传祺token获取成功！', '', `${cookieName}获取成功！请查看日志或弹窗获取Cookie信息。`)
 console.log(`${cookieName}获取成功！`)
 console.log(`Cookie：${headerToken}`)
 
