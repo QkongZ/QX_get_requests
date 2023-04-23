3e953ed10b1a5ef6ccea2bfac31aded10631288b
idodo <96138710+QkongZ@users.noreply.github.com>
2023-04-24 01:08:33 +0800

    Update get_cookie_WXgqcq.js

--- get_ck_js/get_cookie_WXgqcq.js
+++ get_ck_js/get_cookie_WXgqcq.js
@@ -2,19 +2,17 @@
 广汽传祺小程序
 */
 const cookieName = '广汽传祺小程序token'
-const keyToken = 'token'; // 指定要匹配的请求头中的键名
-const regexToken = new RegExp(`${keyToken}\\s*:\\s*([^\s;]+)`, 'i'); // 根据键名创建正则表达式对象
+const regexToken = /token:\s?([^\s]+)/i;
 
-let headerToken = $request.headers[keyToken];
+let headerToken = $request.headers['token']
 
 if (headerToken) {
   let token = regexToken.exec(headerToken)[1];
   console.log(`${cookieName}: Token: ${token}`);
-  $notify(`${cookieName}`, '', `token=${token}`);
+  $notify(`${cookieName}`, '', `token=${headerToken}`);
 } else {
-  $notify(cookieName, '获取Token失败', `请检查请求头中是否包含${keyToken}`);
+  $notify(cookieName, '获取token失败', '请检查请求头中是否包含token');
 }
-
 
 $notify('广汽传祺小程序token获取成功！', '', `${cookieName}获取成功！请查看日志或弹窗获取Cookie信息。`)
 console.log(`${cookieName}获取成功！`)
