60da8d96609305017ae0a4592c0122b4e49ccff2
idodo <96138710+QkongZ@users.noreply.github.com>
2023-04-23 23:05:51 +0800

    Create get_cookie_WXgqcq.js

---
+++ get_ck_js/get_cookie_WXgqcq.js
@@ -0,0 +1,19 @@
+const cookieName = '广汽传祺小程序token'
+
+const regexToken = /token:\s?([^\s]+)/i;
+
+let headerToken = $request.headers['token']
+
+if (headerToken) {
+  let token = regexToken.exec(headerToken)[1];
+  console.log(`${cookieName}: Token: ${token}`);
+  $notify(`${cookieName}`, '', `token=${headerToken}`);
+} else {
+  $notify(cookieName, '获取token失败', '请检查请求头中是否包含token');
+}
+
+$notify('广汽传祺小程序token获取成功！', '', `${cookieName}获取成功！请查看日志或弹窗获取token信息。`)
+console.log(`${cookieName}获取成功！`)
+console.log(`Cookie：${headerToken}`)
+
+$done({})
