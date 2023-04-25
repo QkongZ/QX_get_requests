485f28b35b7fdbcaaaef9c39b83b161eb67a6dc7
idodo <96138710+QkongZ@users.noreply.github.com>
2023-04-26 00:49:13 +0800

    Update get_cookie_cloudYS.js

--- get_ck_js/get_cookie_cloudYS.js
+++ get_ck_js/get_cookie_cloudYS.js
@@ -10,15 +10,18 @@ const headers = $request.headers;
 const tokenMatch = tokenRegex.exec(headers['x-rpc-combo_token']);
 const deviceIdMatch = deviceIdRegex.exec(headers['x-rpc-device_id']);
 
+  let cookieString = `${token};devId=${deviceId}`
 if (tokenMatch && deviceIdMatch) {
   const token = tokenMatch[1];
   const deviceId = deviceIdMatch[1];
-  console.log(`Token: ${token}, Device ID: ${deviceId}`);
-  $notify('匹配到Token和Device ID', '', `Token= ${token};devId=${deviceId}`);
+  console.log(`${cookieName}: ${JSON.stringify(cookie)}`)
+  $notify(`${cookieName}`, '', cookieString)
+} else {
+  $notify(cookieName, '云原神token获取失败', '请检查请求头中是否包含token')
 }
 
 $notify('云原神token获取成功！', '', `${cookieName}获取成功！(请去掉前面的Token=)请查看日志或弹窗获取Cookie信息。`);
 console.log(`${cookieName}获取成功！`);
 console.log(`Token：${headers['x-rpc-combo_token']};devId=${headers['x-rpc-device_id']}`);
 
-$done({});
+$done({})
