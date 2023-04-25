d9278d9344167fc0d5158d2706d2f9e957a3d52b
idodo <96138710+QkongZ@users.noreply.github.com>
2023-04-26 00:22:09 +0800

    Update get_cookie_YSqiqi.js

--- get_ck_js/get_cookie_YSqiqi.js
+++ get_ck_js/get_cookie_YSqiqi.js
@@ -5,15 +5,17 @@
 const cookieName = '原神祈愿🔗';
 const urlRegex = /^https?:\/\/hk4e-api\.mihoyo\.com\/event\/\w+$/i;
 const url = $request.url;
+
+  let urlString = `祈愿URL🔗=${url}}`
 if (urlRegex.test(url)) {
-  console.log(`Matched URL: ${url}`);
-  $notify('匹配到URL', '', `URL=${url}`);
-} else {  
-  $notify('原神祈愿🔗获取失败', '请检查请求头中是否包含Cookie');  
-}  
+  console.log(`${cookieName}: ${JSON.stringify(url)}`)
+  $notify(`${cookieName}`, '', urlString)
+} else {
+  $notify(cookieName, '祈愿🔗获取失败', '请检查请求头中是否包含URL')
+}
 
 $notify('原神祈愿🔗获取成功！', '', `${cookieName}获取成功！请查看日志或弹窗获取七七信息。`)
 console.log(`${cookieName}获取成功！`)
 console.log(`url：${url}`)
 
-$done({});
+$done({})
