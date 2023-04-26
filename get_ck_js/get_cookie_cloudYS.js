6e0c88807bbe00e2f7b9c29be5332073ff58944e
idodo <96138710+QkongZ@users.noreply.github.com>
2023-04-26 09:34:57 +0800

    更新 get_cookie_cloudYS.js

--- get_ck_js/get_cookie_cloudYS.js
+++ get_ck_js/get_cookie_cloudYS.js
@@ -11,9 +11,8 @@ const deviceIdMatch = deviceIdRegex.exec(headers['x-rpc-device_id']);
 if (tokenMatch && deviceIdMatch) {  
   const token = tokenMatch[1];  
   const deviceId = deviceIdMatch[1];
- // console.log(`${cookieName}: ${cookieString}`);  
   console.log(`Token: ${token};devId=${deviceId}`);  
-  $notify(`${cookieName}`, '', `${token};devId=${deviceId}`);  
+  $notify(`${cookieName}`, '', `Token：${headers['x-rpc-combo_token']};devId=${headers['x-rpc-device_id']}`);  
 } //else {  
   //console.log(cookieName, 'Cookie设置失败。请确认 URL、日志或弹窗中是否包含 token');  
  // $notify(cookieName, '云原神token获取失败', '请检查请求头中是否包含token');  
