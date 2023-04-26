3d8f9e5c59c247f087260ad13dd41a5bca552141
idodo <96138710+QkongZ@users.noreply.github.com>
2023-04-26 10:39:44 +0800

    更新 get_cookie_cloudYS.js

--- get_ck_js/get_cookie_cloudYS.js
+++ get_ck_js/get_cookie_cloudYS.js
@@ -4,13 +4,15 @@ const deviceIdRegex = /x-rpc-device_id:\s*([^;\n]+)/i;
   
 const headers = $request.headers;  
 const tokenMatch = tokenRegex.exec(headers['x-rpc-combo_token']);  
-const deviceIdMatch = deviceIdRegex.exec(headers['x-rpc-device_id']);  
-  
+const deviceIdMatch = deviceIdRegex.exec(headers['x-rpc-device_id']); 
+let tokenString = ''; // 初始化弹窗通知中的信息
+
 if (tokenMatch && deviceIdMatch) {  
   const token = tokenMatch[1];  
   const deviceId = deviceIdMatch[1];
   console.log(`Token: ${token};devId=${deviceId}`);  
-  $notify(`${cookieName}`, '', `Token: ${token};devId=${deviceId}`)
+  tokenString = `Token: ${token};devId=${deviceId}`; // 将要显示的信息存储到变量中
+  $notify(`${cookieName}`, '', tokenString); // 在弹窗通知中显示变量的值
 } else {  
   console.log(cookieName, 'Cookie设置失败。请确认 URL、日志或弹窗中是否包含 token');  
   $notify(cookieName, '云原神token获取失败', '请检查请求头中是否包含token');  
@@ -19,5 +21,3 @@ if (tokenMatch && deviceIdMatch) {
 $notify('云原神token获取成功！', '', `${cookieName}获取成功！请查看日志或弹窗获取Cookie信息。`);  
 console.log(`${cookieName}获取成功！`);  
 console.log(`Token：${headers['x-rpc-combo_token']};devId=${headers['x-rpc-device_id']}`);  
-  
-$done({})
