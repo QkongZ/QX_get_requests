19394a8972e11bfea5441dac4b7ce0d11dc5a501
idodo <96138710+QkongZ@users.noreply.github.com>
2023-04-26 10:32:00 +0800

    更新 get_cookie_YSqiqi.js

--- get_ck_js/get_cookie_YSqiqi.js
+++ get_ck_js/get_cookie_YSqiqi.js
@@ -6,16 +6,17 @@ const cookieName = '原神祈愿🔗';
 const urlRegex = /^https?:\/\/hk4e-api\.mihoyo\.com\/event\/\w+$/i;
 const url = $request.url;
 
-  let urlString = `祈愿URL🔗=${url}}`
+let urlString = `祈愿URL🔗=${url}`; // 把多余的 "}" 删掉
+
 if (urlRegex.test(url)) {
-  console.log(`${cookieName}: ${JSON.stringify(url)}`)
-  $notify(`${cookieName}`, '', urlString)
+  console.log(`${cookieName}: ${JSON.stringify(url)}`);
+  $notify(`${cookieName}`, '', urlString);
 } else {
-  $notify(cookieName, '祈愿🔗获取失败', '请检查请求头中是否包含URL')
+  $notify(cookieName, '祈愿🔗获取失败', '请检查请求头中是否包含URL');
 }
 
-$notify('原神祈愿🔗获取成功！', '', `${cookieName}获取成功！请查看日志或弹窗获取七七信息。`)
-console.log(`${cookieName}获取成功！`)
-console.log(`url：${url}`)
+$notify('原神祈愿🔗获取成功！', '', `${cookieName}获取成功！请查看日志或弹窗获取七七信息。`);
+console.log(`${cookieName}获取成功！`);
+console.log(`url：${url}`);
 
-$done({})
+$done({});
