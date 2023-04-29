9f0ed971ed1faf6e1a915c386ee348128ffa7107
idodo <96138710+QkongZ@users.noreply.github.com>
2023-04-30 01:59:52 +0800

    Update get_cookie_gqcq.js

--- get_ck_js/get_cookie_gqcq.js
+++ get_ck_js/get_cookie_gqcq.js
@@ -25,7 +25,7 @@ if (tokenMatch) {
 
 $notify('广汽传祺token获取成功！', '', `${cookieName}获取成功！请查看日志或弹窗获取Cookie信息。`);
 console.log(`${cookieName}获取成功！`);
-console.log(`Token：${token}`)
+console.log(`Token：${headers['token']}`);
 
 setTimeout($done, 1000)
 $done({});
