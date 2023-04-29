58cf8e40f111edf6af36c9bae0ce8c2d6a505265
idodo <96138710+QkongZ@users.noreply.github.com>
2023-04-30 01:30:35 +0800

    Update get_cookie_cloudYS.js

--- get_ck_js/get_cookie_cloudYS.js
+++ get_ck_js/get_cookie_cloudYS.js
@@ -3,7 +3,7 @@ const tokenRegex = /x-rpc-combo_token\s*:\s*(.*?)(;|$)/i;
 const deviceIdRegex = /x-rpc-device_id\s*:\s*([^;\n]+)/i;  
 const headers = $request.headers;  
 
-let tokenString = ''; // 初始化弹窗通知中的信息
+//let tokenString = ''; // 初始化弹窗通知中的信息
 
 if (headers) {
   let Token = {}
@@ -12,9 +12,19 @@ if (headers) {
   if (tokenMatch && deviceIdMatch) {  
     const token = tokenMatch[1];  
     const deviceId = deviceIdMatch[1];
-    console.log(`${cookieName}: ${JSON.stringify(Token)}`);  
-    tokenString = `${token};devId=${deviceId}`; // 将要显示的信息存储到变量中
-    $notify(`${cookieName}`, '', `tokenString`); // 在弹窗通知中显示变量的值
+    console.log(`${cookieName}: Token=${token};devId=${deviceId}`);  
+    let tokenString = `${token};devId=${deviceId}`; // 将要显示的信息存储到变量中
+    $notify(`${cookieName}`, '', tokenString); // 在弹窗通知中显示变量的值
+
+
+    // 打印cookie中所有的键值对
+    let cookieValues = []
+    for (const key in $response.cookies) {
+      cookieValues.push(`${key}: ${$request.headers.Cookie[key]}`)
+    }
+    console.log(`Cookie: ${cookieValues.join(', ')}`)
+
+
   } else {  
     //console.log(cookieName, 'Cookie设置失败。请确认 URL、日志或弹窗中是否包含 token');  
     $notify(cookieName, '云原神token获取失败', '请检查请求头中是否包含token');  
