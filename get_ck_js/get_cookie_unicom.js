a9ba412fa56cabb3c4c28ffb89cf54d4b7717162
idoz <96138710+QkongZ@users.noreply.github.com>
2023-05-06 04:50:41 +0800

    Update get_cookie_unicom.js

--- get_ck_js/get_cookie_unicom.js
+++ get_ck_js/get_cookie_unicom.js
@@ -1,20 +1,22 @@
-const cookieName = '联不通token';
-const notifyName = '🍪Token🍪'
-const body = $request.body;
-const regexToken = /token_online\s*=\s*([^&;]+)/i; // 匹配token_online=xxx的值，不包含&和;
+const cookieName = '联不通token🍭';
+const notifyName = 'Token🍪'
+const body = typeof $request.body === 'string' ? $request.body : JSON.stringify($request.body, null, 2);
+
+const key1 = 'token_online'
+const regex1 = new RegExp(`${key1}=\\s*([^&;\\n]+)`, 'i'); // 匹配token_online=xxx的值，不包含&和;
 
 if (body) {
-  const match = regexToken.exec(body);
+  const match = regex1.exec(body);
   if (match) {
     const token = match[1];
     console.log(`${notifyName}: ${token}`);
-    $notify(`匹配到 ${cookieName} `, '', `${notifyName}=${token}`);
+    $notify(`🎉${cookieName} 获取成功！`, '', `${notifyName}=${token}`);
   }
 }
 
-$notify(`${notifyName} 获取成功！`, '', `${cookieName}获取成功！请查看日志或弹窗获取Cookie信息。`)
-console.log(`${cookieName}获取成功！`)
-console.log(`🔔输出完整${notifyName}：${body}`)
+$notify(`${cookieName}获取成功！`, '', `${cookieName}获取成功！请查看弹窗匹配值或日志查看完整值。`);
+console.log(`${cookieName}获取成功！`);
+console.log(`🔔输出完整请求值：${body}\n`);
 
-setTimeout($done, 1000)
-$done({})
+setTimeout($done, 1000);
+$done({});
