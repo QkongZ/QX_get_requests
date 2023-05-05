9a052873a51404b67d945cf3d8f9e26d8a814883
idoz <96138710+QkongZ@users.noreply.github.com>
2023-05-06 03:49:01 +0800

    Update get_cookie_WXgqcq.js

--- get_ck_js/get_cookie_WXgqcq.js
+++ get_ck_js/get_cookie_WXgqcq.js
@@ -3,27 +3,21 @@
 广汽传祺小程序
 */
 const cookieName = '广汽传祺小程序token';
-const notifyName = '🍪Token🍪'
+const notifyName = '🍪Token🍪';
 
-const key1 = 'token'
-const regex1 = new RegExp(`${key1}[=:]\\s*([^;\\n]+)`, 'i');
 const headers = $request.headers;
+const match = $request.headers['token'];
 
-if (headers) {
-  const match = regex1.exec(headers);
-  if (match) {
-    const Token = match[1];
-    console.log(`${notifyName}: ${Token}`);
-    $notify(`🎉${cookieName} 获取成功！🎉`, '', `${notifyName}=${Token}`);
-  } //else {
-    //$notify(`${cookieName}`, `获取${notifyName}失败`, `请检查请求头中是否包含${notifyName}`);
-  //}
+if (match) {
+  console.log(`${notifyName}: ${match}`);
+  $notify(`🎉${cookieName} 获取成功！🎉`, '', `${notifyName}=${match}`);
+} else {
+  $notify(`${cookieName}`, `获取${notifyName}失败`, `请检查请求头中是否包含${notifyName}`);
 }
-
 
 $notify(`${cookieName}获取成功！`, '', `${cookieName}获取成功！请查看弹窗匹配值或日志查看完整值。`);
 console.log(`${cookieName}获取成功！`);
-console.log(`🔔输出完整${notifyName}：${headers['token']}`);
+console.log(`🔔输出完整请求值：${headers}`);
 
-setTimeout($done, 1000)
-$done({})
+setTimeout($done, 1000);
+$done({});
