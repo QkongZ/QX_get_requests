48729e093d44157c7a99104ed5b6eb04b15c6fa9
idoz <96138710+QkongZ@users.noreply.github.com>
2023-05-06 04:24:45 +0800

    Update get_cookie_cloudYS.js

--- get_ck_js/get_cookie_cloudYS.js
+++ get_ck_js/get_cookie_cloudYS.js
@@ -1,22 +1,21 @@
-const cookieName = '云原神token';   
-const notifyName = '🍪Token🍪';
+const cookieName = '🍭云原神token';   
+const notifyName = '🍪Token';
 
 const headers = $request.headers;
 const match1 = $request.headers['x-rpc-combo_token'];
 const match2 = $request.headers['x-rpc-device_id'];
 
-//let tokenString = ''; // 初始化弹窗通知中的信息
 
 if (match1 && match2) {
   console.log(`${notifyName}: ${match1};devId=${match2};`);
-  $notify(`🎉${cookieName} 获取成功！🎉`, '', `${notifyName}=${match1};devId=${match2};`);
+  $notify(`🎉${cookieName} 获取成功！`, '', `${notifyName}=${match1};devId=${match2};`);
 } else {
-  $notify(`${cookieName}`, `获取${notifyName}失败`, `请检查请求头中是否包含${notifyName}`);
+  $notify(`${cookieName}`, `❌获取${notifyName}失败`, `请检查请求头中是否包含${notifyName}`);
 }
 
 $notify(`${cookieName}获取成功！`, '', `${cookieName}获取成功！请查看弹窗匹配值或日志查看完整值。`);
 console.log(`${cookieName}获取成功！`);
-console.log(`🔔输出完整请求值：${JSON.stringify(headers, null, 2)}`);
+console.log(`🔔输出完整请求值：${JSON.stringify(headers, null, 2)}\n`);
 
 
 setTimeout($done, 1000)
