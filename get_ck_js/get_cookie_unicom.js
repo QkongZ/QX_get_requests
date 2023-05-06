84b7057bafd7ee3224261a0aac2cf389bde7eac8
idoz <96138710+QkongZ@users.noreply.github.com>
2023-05-06 18:58:49 +0800

    更新 get_cookie_unicom.js

--- get_ck_js/get_cookie_unicom.js
+++ get_ck_js/get_cookie_unicom.js
@@ -16,7 +16,7 @@ if (body) {
 
 $notify(`${cookieName}获取成功！`, '', `${cookieName}获取成功！请查看弹窗匹配值或日志查看完整值。`);
 console.log(`${cookieName}获取成功！`);
-console.log(`🔔输出完整请求值：${body}\n`);
+console.log(`🔔输出完整请求值：\n${body}\n`);
 
 setTimeout($done, 1000);
 $done({});
