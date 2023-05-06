44744e7efefd86353d5679249141807764b2a9be
idoz <96138710+QkongZ@users.noreply.github.com>
2023-05-06 18:58:11 +0800

    更新 get_cookie_duoduogy.js

--- get_ck_js/get_cookie_duoduogy.js
+++ get_ck_js/get_cookie_duoduogy.js
@@ -23,7 +23,7 @@ if (headerCookie) {
 
 $notify(`${cookieName}获取成功！`, '', `${cookieName}获取成功！请查看弹窗匹配值或日志查看完整值。`);
 console.log(`${cookieName}获取成功！`);
-console.log(`🔔输出完整请求值：${JSON.stringify(headerCookie, null, 2)}\n`)
+console.log(`🔔输出完整请求值：\n${JSON.stringify(headerCookie, null, 2)}\n`)
 
 setTimeout($done, 1000)
 $done({})
