d965f0b8f3a4cb2fca962b97ecb63675b38311da
idoz <96138710+QkongZ@users.noreply.github.com>
2023-05-07 19:40:43 +0800

    更新 get_cookie_YSqiqi.js

--- get_ck_js/get_cookie_YSqiqi.js
+++ get_ck_js/get_cookie_YSqiqi.js
@@ -5,20 +5,20 @@
 const cookieName = '原神祈愿🔗';
 const notifyName = 'URL🍪';
 
-const urlRegex = /^https?:\/\/hk4e-api\.mihoyo\.com\/event\/\w+$/i;
-const Url = $request.url;
+const 1Regex = /^https?:\/\/hk4e-api\.mihoyo\.com\/event\/\w+$/i;
+const url = $request.url;
 
 
-if (urlRegex.test(Url)) {
-  console.log(`${cookieName}: ${Url}`);
-  $notify(`${cookieName}`, '', `${Url}`;
+if (1Regex.test(url)) {
+  console.log(`${cookieName}: ${url}`);
+  $notify(`${cookieName}`, '', `${url}`;
 } else {
   $notify(`❌${cookieName}获取失败`, '', `请检查请求头中是否包含${notifyName}`);
 }
 
 $notify(`🎉${cookieName}获取成功！`, '', `${notifyName}获取成功！请查看日志或弹窗获取七七信息。`);
 console.log(`${cookieName}获取成功！`);
-console.log(`${notifyName}：\n${Url}\n`);
+console.log(`${notifyName}：\n${url}\n`);
 
 setTimeout($done, 1000)
 $done({})
