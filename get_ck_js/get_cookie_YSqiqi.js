d0813da611c778af6409a431b6f17aa50789f130
idoz <96138710+QkongZ@users.noreply.github.com>
2023-05-27 13:58:31 +0800

    更新 get_cookie_YSqiqi.js

--- get_ck_js/get_cookie_YSqiqi.js
+++ get_ck_js/get_cookie_YSqiqi.js
@@ -5,12 +5,12 @@
 const cookieName = '原神祈愿🔗';
 const notifyName = 'URL🍪';
 
-const regex1 = /^https?:\/\/hk4e-api\.mihoyo\.com\/event\/gacha_info\/api\w+$/i;
-const url = $request.url;
+const regex1 = /^https?:\/\/hk4e-api\.mihoyo\.com\/event\/gacha_info\/api\/getGachaLog?\w+$/i;
+const url1 = $request.url;
 
 
-if (regex1.test(url)) {
-  console.log(`${cookieName}: ${url}`);
+if (regex1.test(url1)) {
+  console.log(`${cookieName}: ${url1}`);
   $notify(`🎉${cookieName}获取成功`, '', `${notifyName}=${url}`;
 } else {
   $notify(`❌${cookieName}获取失败`, '', `请检查请求头中是否包含${notifyName}`);
@@ -18,7 +18,7 @@ if (regex1.test(url)) {
 
 $notify(`${cookieName}获取成功！`, '', `${notifyName}获取成功！请查看日志或弹窗获取七七信息。`);
 console.log(`${cookieName}获取成功！`);
-console.log(`🔔完整值${notifyName}：\n${url}\n`);
+console.log(`🔔完整值${notifyName}：\n${url1}\n`);
 
 setTimeout($done, 1000)
 $done({})
